d19c7b89e7144e33900c7c088cad2a2755707913
Jhonatan Core <jhon.corevill@gmail.com>
2023-12-03 16:41:14 +0100

    a

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,15 @@
 'user strict';
 
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 //local mysql db connection
 const dbConn = mysql.createConnection({
-  host     : 'localhost',
+  host     : 'viaduct.proxy.rlwy.net',
   user     : 'root',
-  password : '',
-  database : 'finanzas_personales'
+  password : 'BE-Agd5Ch6e2cCF1aCbGdd3e-52d-D5B',
+  database : 'railway',
+  port: 23943,
+  multipleStatements: true
 });
 dbConn.connect(function(err) {
   if (err) throw err;
